7fb5e49bcccbb0d7f7bfc85e0f351e50c340f762
marcos <marcosjmedina@gmail.com>
2021-09-22 18:38:25 -0300

    clase 1 js

--- js/1-holamundo.js
+++ js/1-holamundo.js
@@ -4,4 +4,41 @@ multiples lineas*/
 
 //mostrar un mensaje en la consola
 
-console.log('hola mundo')
+console.log('hola mundo');
+
+//mostrar un mensaje en la pagina html
+
+document.write("este es un mensaje");
+
+//mostrar un mensaje en una ventana emergente
+
+//alert('Esto es una alerta o ventana emergente');
+
+//crear o declarar variables (var - let- const)
+
+ //"nombre de la cajita" estamos etiquetando la caja, o lo que esperamos guardar dentro
+
+ var alumno = 'Rodrigo';
+ let edad = 27; //los numeros no requiere comilla de string
+
+ document.write('<br> Alumno: '+ alumno );
+ document.write('<br> Edad: '+ edad );
+
+ alumno = 'Marta';
+ document.write('<br> Alumno: '+ alumno );
+
+ //pedir a un usuario que ingrese un nombre de alumno
+
+ alumno = prompt('Ingrese un nombre de un alumno');
+ document.write('<br> Alumno: '+ alumno );
+
+ edad = prompt('Ingrese una edad de un alumno');
+ document.write('<br> Edad: '+ edad );
+
+ const url = 'https://twitter.com/home' ;
+ document.write('<br> URL: '+ url);
+
+ let numero1 = 20;
+ let numero2 = parseInt(prompt('ingrese un numero'));
+
+ document.write('<br> Resultado:' + (numero1 + numero2));
